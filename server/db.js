55e42b4b150e5d6a75a9e5e1154859c04afc81d4
Ashish <jadhav.ashish2002@gmail.com>
2024-04-20 08:01:10 +0530

    added test.js

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb+srv://ashish:ashish@inventorymanagement.atnyodp.mongodb.net/InventoryManagement";
+const mongoURI = "mongodb+srv://ashish:ashish@inventorymanagement.atnyodp.mongodb.net/?retryWrites=true&w=majority&appName=InventoryManagement";
 
 const connectToMongo = async () => {
         await mongoose.connect(mongoURI);
---
+++ server/test.js
@@ -0,0 +1,38 @@
+[
+    {
+      "date": "2024-04-20 03:00:00",
+      "humidity": 70,
+      "temperature": 302.15,
+      "climate": "Rain",
+      "weather_description": "light rain"
+    },
+    {
+      "date": "2024-04-20 06:00:00",
+      "humidity": 64,
+      "temperature": 303,
+      "climate": "Clouds",
+      "weather_description": "few clouds"
+    },
+    {
+      "date": "2024-04-20 09:00:00",
+      "humidity": 58,
+      "temperature": 303.3,
+      "climate": "Clouds",
+      "weather_description": "few clouds"
+    },
+    {
+      "date": "2024-04-20 12:00:00",
+      "humidity": 54,
+      "temperature": 303.8,
+      "climate": "Clear",
+      "weather_description": "clear sky"
+    },
+    {
+      "date": "2024-04-20 15:00:00",
+      "humidity": 58,
+      "temperature": 303.18,
+      "climate": "Clear",
+      "weather_description": "clear sky"
+    }
+  ]
+  
